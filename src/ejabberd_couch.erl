cd6227974120f8b8dc0d25360a4c78b588eaebbe
Christopher 'Tofu' Zorn <tofu@stanziq.com>
2009-06-10 09:31:05 -0400

    clean up some of the api

--- src/ejabberd_couch.erl
+++ src/ejabberd_couch.erl
@@ -2,7 +2,12 @@
 
 -author("tofu@collecta.com").
 
--export([init/1]).
+-export([init/1,
+	 doc_delete/3,
+	 doc_update/3,
+	 doc_create/3,
+	 doc_get/2
+	]).
 
 init(Opts) ->
     %% set up and load ecouch
@@ -10,9 +15,50 @@ init(Opts) ->
     application:set_env(?MODULE, port, gen_mod:get_opt(port, Opts, "5984")),
     application:set_env(?MODULE, user, gen_mod:get_opt(user, Opts, none)),
     application:set_env(?MODULE, pass, gen_mod:get_opt(pass, Opts, none)),
-
+    application:start(ibrowse),
     ok.
 
+get_url(Name, Id) ->
+    CouchUrl = "http://"++get_opt(host)++":"++get_opt(port)++"/",
+    CouchUrl ++ "/" ++ Name ++ "/" ++ Id.
+    
+send_request(Url, Type, Doc) ->
+    Options = [], %% add username and password
+    EncDoc = case Doc of 
+		 "" ->
+		     [];
+		 _ ->
+		     rfc4627:encode(Doc)
+	     end,
+    case catch ibrowse:send_req(Url,
+                                [{"Content-Type", "application/json"}],
+                                Type, EncDoc,
+				Options) of
+        {ok, "200", _Headers, Payload} = Res->
+	    rfc4627:decode(Payload);
+        {ok, "201", _Headers, Payload} = Res->
+	    rfc4627:decode(Payload);
+        Error ->
+            ?ERROR("~p sending ~p to ~p~n",
+		   [Error, Doc, Url]),
+            throw({error, storage_error, Error})
+    end.
+
+doc_delete(Name, Id) ->
+    Url = get_url(Name, Id),
+    send_request(Url, delete, []).
+
+doc_update(Name, Id, Value) ->
+    Url = get_url(Name, Id),
+    send_request(Url, post, Value).
+
+doc_create(Name, Id, Value) ->
+    Url = get_url(Name, Id),
+    send_request(Url, put, Value).
+
+doc_get(Name, Id) ->
+    Url = CouchUrl ++ "/" ++ Id,
+    send_request(Url, get, []).
 
 
 get_opt(Opt, Default) ->
